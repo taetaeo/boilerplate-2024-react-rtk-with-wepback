7eecbae3c1d7a6b1b30d1e6176737ac3943d5221
ohtaekwon <ohtk1119@gmail.com>
2024-03-21 13:59:16 +0900

    feat implement babel config

--- bable.config.js
+++ bable.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-typescript'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@commons': './src/_commons',
+          '@mocks': './src/_mocks',
+          '@components': './src/components',
+          '@configs': './src/configs',
+          '@dto': './src/dto',
+          '@hooks': './src/hooks',
+          '@lib': './src/lib',
+          '@modules': './src/modules',
+          '@redux': './src/redux',
+          '@views': './src/views',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+          _types: './src/types',
+        },
+      },
+    ],
+  ],
 };
