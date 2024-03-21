9b084dd7438c132ae0ff7c0d6bab736a09ea133b
ohtaekwon <ohtk1119@gmail.com>
2024-03-21 14:00:14 +0900

    feat implement temporary app, index

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+
+// Configs
+import routerConfig from '@configs/router.config';
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={routerConfig} />
+    </>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+// Store
+import store from '@redux/store';
 import App from './app';
 
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('@mocks/browser/worker');
+  return worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+
+enableMocking().then(() => {
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  );
+});
